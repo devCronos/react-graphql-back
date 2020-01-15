637ba48ef2673ffd51dd4e2a675b87926a90cdbc
Claudiu <claudiu.bargianu@gmail.com>
2020-01-15 17:51:13 +0200

    added auth and jwt token sign in

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO check if they are logged in
@@ -29,6 +32,53 @@ const mutations = {
         // check for permission
         // delete
         return ctx.db.mutation.deleteItem({where}, info)
+    },
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+        // hash password 1way. bcrypt is async. salt makes it unique
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the db
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] }
+            }
+        },info)
+        // create the JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set the JWT as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000*60*60*24*365
+        });
+        // return the user
+        return user;
+    },
+    async signin(parent,{email, password},ctx,info){
+        // check if user exists
+        const user = await ctx.db.query.user({where: {email}});
+        if(!user){
+            throw new Error(`Noy user found for email ${email}`);
+        }
+        // check password
+        const valid = await bcrypt.compare(password, user.password);
+        if(!valid){
+            throw new Error('Invalid password');
+        }
+        // generate the JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set the cookie with the token
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000*60*60*24*365
+        });
+        // return the user
+        return user;
+    },
+    signout(parent, args, ctx, info){
+        ctx.response.clearCookie('token');
+        return {message: 'Goodbye!'};
     }
 };
 
